0ae7df5a2bc4a09680b9488fe369153347006722
YangHaoChen <x73901@live.com>
2022-06-21 20:28:23 +0800

    UnlitShader

---
+++ Octopath_Traveler_Lighting/SpriteRenderFeature.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+namespace SpriteRender
+{
+    public class SpriteRenderFeature : ScriptableRendererFeature
+    {
+        [System.Serializable]
+        public class SpriteRenderSetting
+        {
+            public RenderPassEvent _passEvent = RenderPassEvent.AfterRenderingTransparents;
+            public Material _mat;
+            public int matPassIdx = -1;
+
+            public FilterMode _filterMode = FilterMode.Bilinear;
+        }
+
+        public class SpriteRenderPass : ScriptableRenderPass
+        {
+            public SpriteRenderPass ( RenderPassEvent passEvent, Material mat, int passInt, string tag )
+            {
+                
+            }
+
+            public override void Execute ( ScriptableRenderContext context, ref RenderingData renderingData )
+            {
+            }
+        }
+
+        public override void AddRenderPasses ( ScriptableRenderer renderer, ref RenderingData renderingData )
+        {
+        }
+
+        public override void Create ()
+        {
+        }
+    }
+
+}
